0ccb5a69ee243b6ab79cf7372105ca8da6988550
Vivek Nair <vivek.nair@kellogg.northwestern.edu>
2025-02-19 23:35:31 +0000

    Solved the problem set

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,23 @@
 class TransactionsController < ApplicationController
   def index
+    @transactions = Transaction.all.includes(:from_user, :to_user)
+    @balances = calculate_balances
     render :template => "transactions/index"
+  end
+
+  private
+
+  def calculate_balances
+    balances = Hash.new(0)
+
+    Transaction.all.each do |transaction|
+      to_user = User.find_by(id: transaction.to_user_id)
+      from_user = User.find_by(id: transaction.from_user_id)
+
+      balances[to_user.name] += transaction.amount if to_user
+      balances[from_user.name] -= transaction.amount if from_user
+    end
+
+    balances
   end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ApplicationRecord
-  # connects to transactions table
+  belongs_to :from_user, class_name: "User", foreign_key: "from_user_id", optional: true
+  belongs_to :to_user, class_name: "User", foreign_key: "to_user_id"
 end
